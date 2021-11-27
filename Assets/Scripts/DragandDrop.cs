693f2cbebc203c009e814ef4fa3d72a740370885
Patrickfaessler <32198459+Patrickfaessler@users.noreply.github.com>
2021-11-27 11:19:10 +0100

    Fix

--- Assets/Scripts/DragandDrop.cs
+++ Assets/Scripts/DragandDrop.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class DragandDrop : MonoBehaviour
 {
@@ -8,101 +11,185 @@ public class DragandDrop : MonoBehaviour
     public GameObject closestPart;
     public GameObject spaceship;
     public GameObject inventory;
-
-    private bool InInventory;
-    private Vector3 Inpos;
-
-   //bad practice need change on full inventory implementation
-   private void OnTriggerStay2D(Collider2D other)
-    {
-        InInventory = true;
-        Inpos = transform.position;
-    }
+    private bool _inInventory;
+    private GameObject _snapShadow;
     
-    private void OnTriggerExit2D(Collider2D other)
+   private void OnTriggerEnter2D(Collider2D other)
     {
-        InInventory = false;
+        if (other.gameObject == inventory)
+        {
+            for (int i=0; i<transform.childCount; i++) {
+                Destroy(transform.GetChild(i).gameObject.GetComponent<Docking>());
+            }
+            _inInventory = true;
+        }
+         
+    }
+   
+   private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == inventory)
+        {
+            _inInventory = false;
+
+            for (int i=0; i<transform.childCount; i++) {
+                if(!transform.GetChild(i).gameObject.GetComponent<Docking>())
+                    transform.GetChild(i).gameObject.AddComponent<Docking>();
+            }
+        }
+           
     }
 
     private void Start()
     {
-        spaceship = GameObject.Find("MainBody");
-        inventory = GameObject.Find("Inventory");
+        spaceship = GameObject.Find("Spaceship(Clone)");
+        inventory = GameObject.Find("Building Inventory");
     }
+
     private void OnMouseDown()
     {
+        Debug.Log(gameObject.name);
+        InitShadow();
+        part = GameObject.FindGameObjectsWithTag("DockEmpty");
         this.tag = "Part";
-        //this.transform.SetParent(inventory.transform);
-    }
+            //this.transform.SetParent(inventory.transform);
+        }
+    
 
     void OnMouseDrag()
     {
-        transform.position = GetMousePos();
-    }
-    private void OnMouseUp()
-    {
-        if(!this.InInventory)
+        if (true)
         {
-            var otherbody = this.GetClosestEnemy();
-            var temp = (this.transform.position - otherbody.transform.position).normalized;
-            if (Math.Abs(temp.x) <= Math.Abs(temp.y))
+            transform.position = GetMousePos();
+            Transform transform1;
+            if (this.gameObject.name != "Body(Clone)")
             {
-                if (temp.y > 0)
+                if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
                 {
-                    this.transform.position = new Vector3(otherbody.transform.position.x,
-                        otherbody.transform.position.y + transform.localScale.y);
+                    Debug.Log("90°");
+                    Vector3 newRotation = new Vector3(0, 0, 90);
+                    transform1 = this.transform;
+                    transform1.eulerAngles = transform1.eulerAngles + newRotation;
                 }
-                else
+                else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
                 {
-                    this.transform.position = new Vector3(otherbody.transform.position.x,
-                        otherbody.transform.position.y - transform.localScale.y);
-                }
-            }
-            else
-            {
-                if (temp.x > 0) 
-                {
-                    this.transform.position = new Vector3(otherbody.transform.position.x + this.transform.localScale.x,
-                        otherbody.transform.position.y);
-                }
-                else
-                {
-                    this.transform.position = new Vector3(otherbody.transform.position.x - this.transform.localScale.x,
-                        otherbody.transform.position.y);
+                    Vector3 newRotation = new Vector3(0, 0, -90);
+                    transform1 = this.transform;
+                    transform1.eulerAngles = transform1.eulerAngles + newRotation;
                 }
             }
 
-            this.tag = "Ship";
-            this.transform.SetParent(this.spaceship.transform);
+            RenderShadow();
+
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        DestroyShadow();
+        if(!_inInventory){ 
+            Snap();
         }
         else
-            this.transform.position = this.Inpos;
+        {
+            transform.position = inventory.transform.position;
+        }
     }
     
     Vector3 GetMousePos()
     {
         var mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousPos.z = 0;
-        return mousPos;
+            mousPos.z = 0;
+            return mousPos;
     }
+    
 
-    GameObject GetClosestEnemy()
+    private Tuple<GameObject,Transform> GetClosestDockingPoint()
     {
-        part = GameObject.FindGameObjectsWithTag("Ship");
+        Transform dockingpoint=null;
+        GameObject[] children = new GameObject[transform.childCount];
+        for (int i=0; i<transform.childCount; i++) {
+            children[i]=transform.GetChild(i).gameObject;
+        }
+        part = GameObject.FindGameObjectsWithTag("DockEmpty");
+        IEnumerable<GameObject>possibledocks = part.Except(children);
         float closestDistanceSqr = Mathf.Infinity;
         Vector3 currentPosition = transform.position;
-        foreach (GameObject temp in part)
+        foreach (Transform child in transform)
         {
-            Vector3 directionToTarget = temp.transform.position - currentPosition;
-            float dSqrToTarget = directionToTarget.sqrMagnitude;
-            if (dSqrToTarget < closestDistanceSqr)
+
+            foreach (GameObject temp in possibledocks )
             {
-                closestDistanceSqr = dSqrToTarget;
-                closestPart = temp;
+                Vector3 localrotavec = -(this.transform.rotation * child.localPosition);
+                if((temp.transform.parent.rotation*temp.transform.localPosition)!=localrotavec)
+                   continue;
+                if(!temp.transform.parent.CompareTag("Ship"))
+                    continue;
+                // Debug.DrawLine(temp.transform.position,child.position,Color.red,2.5f);
+                Vector3 directionToTarget = temp.transform.position - child.position;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+                if (dSqrToTarget < closestDistanceSqr)
+                {
+                    closestDistanceSqr = dSqrToTarget;
+                    closestPart = temp;
+                    dockingpoint = child;
+                }
+            }
+            
+        }
+        return new Tuple<GameObject, Transform>(closestPart,dockingpoint);
+    }
+    
+    
+    private void Snap()
+    {
+        Tuple<GameObject,Transform> docking =  GetClosestDockingPoint();
+        var transform1 = this.transform;
+        var position = docking.Item1.transform.position;
+        Vector3 temp = (transform1.position - position).normalized;
+        var localPosition = docking.Item2.localPosition;
+        Vector3 locpos = localPosition;
+        Vector3 localposrotat= transform1.rotation * localPosition;
+        transform1.position = position-localposrotat;
+        //this.tag = "Ship"; 
+        this.transform.SetParent(spaceship.transform);
+    }
+    private void InitShadow()
+    {
+       
+        _snapShadow = Instantiate(this.gameObject, inventory.transform, true) as GameObject;
+        foreach (var comp in _snapShadow.GetComponents<Component>())
+        {
+            if (!(comp is Transform || comp is SpriteRenderer))
+            {
+                Destroy(comp);
             }
         }
+        for (int i=0; i<_snapShadow.transform.childCount; i++) {
+            Destroy(_snapShadow.transform.GetChild(i).gameObject);
+        }
+        _snapShadow.tag = "Untagged";
+        Color tmp = _snapShadow.GetComponent<SpriteRenderer>().color;
+        tmp.a = 0.4f;
+        _snapShadow.GetComponent<SpriteRenderer>().color = tmp;
+    }
+    private void RenderShadow()
+    {
+        _snapShadow.transform.rotation = this.gameObject.transform.rotation;
+        SpriteRenderer _renderer = _snapShadow.gameObject.GetComponent<SpriteRenderer>();
+        Tuple<GameObject,Transform> docking =  GetClosestDockingPoint(); 
+        var transform1 = this.transform;
+        var position = docking.Item1.transform.position;
+        Vector3 temp = (transform1.position - position).normalized;
+        var localPosition = docking.Item2.localPosition;
+        Vector3 locpos = localPosition;
+        Vector3 localposrotat= transform1.rotation * localPosition;
+        _renderer.transform.position = position-localposrotat;
+    }
 
-        return closestPart;
+    private void DestroyShadow()
+    {
+        Destroy(_snapShadow);
     }
 }
  
