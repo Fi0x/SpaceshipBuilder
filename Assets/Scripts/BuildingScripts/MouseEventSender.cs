58a1e73e94943c1ac3d8722e6877bff1430eaba8
Fi0x <felpech@aol.com>
2021-12-13 22:40:24 +0100

    Fixed mouse events. Drag and drop now works

--- Assets/Scripts/BuildingScripts/MouseEventSender.cs
+++ Assets/Scripts/BuildingScripts/MouseEventSender.cs
@@ -6,33 +6,42 @@ namespace BuildingScripts
 {
     public class MouseEventSender : MonoBehaviour
     {
-        [SerializeField] private List<CreatePart> childrenToReceiveEvents;
+        private DragAndDrop _correctPart;
         
         private void OnMouseDown()
         {
-            this.childrenToReceiveEvents.ForEach(c => c.CurrentChild.OnMouseDown());
+            this._correctPart = this.GetRaycastChild();
+            this._correctPart.OnMouseDown();
         }
 
         private void OnMouseDrag()
         {
-            this.childrenToReceiveEvents.ForEach(c => c.CurrentChild.OnMouseDrag());
+            if(this._correctPart == null)
+                this._correctPart = this.GetRaycastChild();
+            
+            this._correctPart.OnMouseDrag();
         }
 
         private void OnMouseUp()
         {
-            this.childrenToReceiveEvents.ForEach(c => c.CurrentChild.OnMouseUp());
+            if(this._correctPart == null)
+                this._correctPart = this.GetRaycastChild();
+            
+            this._correctPart.OnMouseUp();
+
+            this._correctPart = null;
         }
 
-        private IEnumerable<DragAndDrop> GetValidChild()
+        private DragAndDrop GetRaycastChild()
         {
             var hitTransforms = new List<Transform>();
             Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), new Vector3(0, 0, 1))
                 .ToList()
                 .ForEach(hit => hitTransforms.Add(hit.transform));
-            
+
             var childComponents = this.transform.GetComponentsInChildren<DragAndDrop>();
             var validChild = childComponents.ToList().Where(child => hitTransforms.Contains(child.transform));
-            return validChild;
+            return validChild.First();
         }
     }
 }
